462400c14cba649e64467a225ce8590599b352d9
maniRhombuz <manish@rhombuz.io>
2023-07-20 21:17:38 +0530

    Loading animation v1

--- scripts/destination.js
+++ scripts/destination.js
@@ -2,10 +2,13 @@ fetch(`http://localhost:3000/destination`).then((res) => {
     return res.json();
 }).then((data) => {
     console.log(data);
-    displayData(data);
+    setTimeout(function() {
+        displayData(data);
+    }, 1000)
 })
 
 let mainSection = document.querySelector(".card-container")
+
 
 function displayData(data) {
     mainSection.innerHTML = null;
@@ -36,13 +39,25 @@ function displayData(data) {
         let price = document.createElement("h3");
         price.setAttribute("class", "price");
         price.innerText = `Rs. ${ele.price}`;
+        let button = document.createElement("button");
+        button.setAttribute("class", "btn");
+        button.innerText = "Add to Cart"
 
         imgDiv.append(img)
-        card.append(location, tagline, imgDiv, about, listHead, price);
+        card.append(location, tagline, imgDiv, about, listHead, price, button);
         mainSection.append(card)
 
     })
 }
+
+{/* <button class="button-82-pushable" role="button">
+  <span class="button-82-shadow"></span>
+  <span class="button-82-edge"></span>
+  <span class="button-82-front text">
+    Button 82
+  </span>
+</button> */}
+
 // {
 //     "id": 1,
 //     "location": "Kerala",
